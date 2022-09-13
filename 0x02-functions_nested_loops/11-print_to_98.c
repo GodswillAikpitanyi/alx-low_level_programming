841371828702e12f3370bf3a28d53aaf0aa14de2
Godswill Aikpitanyi <godswillaikpitanyi@gmail.com>
2022-09-13 19:37:05 +0000

    task 11 update

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,21 +13,22 @@ void print_to_98(int n)
 {
 	if (n < 98)
 	{
-		for (n = n; n < 98; n++)
+		while (n  < 98)
 		{
 
 			printf("%d, ", n);
-			printf("%d\n", 98);
+			n++
 		}
 	}
 
-	else
+	else if
 	{
-		for (n = n; n > 98; n--)
+		while (n > 98)
 		{
 
 			printf("%d, ", n);
-			printf("%d\n", 98);
+			n--
 		}
 	}
+	print("98\n");
 }
